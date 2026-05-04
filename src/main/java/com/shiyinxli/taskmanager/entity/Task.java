168e13656b76bbd078796f61814d8bba68dd5d62
Shiyin Li <lsyin85@gmail.com>
2026-05-04 10:11:59 +0200

    feat: add Task entity with status enum

---
+++ src/main/java/com/shiyinxli/taskmanager/entity/Task.java
@@ -0,0 +1,29 @@
+package com.shiyinxli.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDate dueDate;
+}
---
+++ src/main/java/com/shiyinxli/taskmanager/entity/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.shiyinxli.taskmanager.entity;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
