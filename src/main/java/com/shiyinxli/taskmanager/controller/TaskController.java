409f914e3c364a7cfa423c5c21c8467ceb76e03e
Shiyin Li <lsyin85@gmail.com>
2026-05-05 21:16:17 +0200

    feat: create TaskController with create and get endpoints

---
+++ src/main/java/com/shiyinxli/taskmanager/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.shiyinxli.taskmanager.controller;
+
+import com.shiyinxli.taskmanager.entity.Task;
+import com.shiyinxli.taskmanager.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+}
